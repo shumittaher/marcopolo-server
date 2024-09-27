dc55258e5200e545f3acdb3f0b967055675a04bf
shumit taher <shumitt@gmail.com>
2024-09-27 21:57:28 +0600

    fixed db access

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.egg9z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.egg9z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -87,6 +87,7 @@ async function run() {
 
 
 app.get('/', (req, res) => {
+    console.log('request hit')
     res.send('Running marco polo Server')
 })
 
